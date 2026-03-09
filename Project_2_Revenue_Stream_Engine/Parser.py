004b5eb8b7516fe7b765959515f6cd87bbfdf947
Wolfgang Welch <wolfgangwelch.official@gmail.com>
2026-03-09 16:12:59 -0700

    Create Parser.py

---
+++ Project_2_Revenue_Stream_Engine/Parser.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import io
+
+def parse_uploaded_file(uploaded_file):
+
+    filename = uploaded_file.name
+
+    if filename.endswith(".csv"):
+        return pd.read_csv(uploaded_file)
+
+    if filename.endswith(".xlsx"):
+        return pd.read_excel(uploaded_file)
+
+    raise ValueError("Unsupported file type")
